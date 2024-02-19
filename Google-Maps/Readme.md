a648f1447988f78cb3fcbf07a5f9b2aa3e927996
mai <maitsuji@g.ecc.u-tokyo.ac.jp>
2024-02-19 12:24:46 +0900

    google-map with street 2024

---
+++ Google-Maps/Readme.md
@@ -0,0 +1,14 @@
+![picture 0](../images/2843f16e017ae31c892780558c791bc914ca656522acf07c91dbe918139c1d8c.png)  
+When two points (start point and goal point) are specified with the mouse
+The route to the start point and the goal point will be displayed.
+The street view of the 10 points along the route is displayed in order, one second at a time.
+[How to use]
+Assign your Google Map API key to YOUR_API_KEY and open 'maps.html'.
+
+マウスで２地点（start地点、goal地点）を指定すると
+そこまでの経路が表示されるとともに
+下に経路間の10地点のストリートビューが1秒ずつ順に表示される。
+[使い方]
+Google MapのAPIキーをYOUR_API_KEYに代入しmaps.htmlを開く
+![picture 1](../images/7e860f85485203636999df142ac3cba2ca7fa8cd821892325f3f2b01a03cb02a.png)  
+![picture 2](../images/9aec8247730f9d4749f1cefe60258e8c7c9fae1133f087d54f130f3e53e41e31.png)  
---
+++ Google-Maps/script.js
@@ -0,0 +1,102 @@
+
+
+let start, end;
+let clicks = 0;
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: {lat: 35.66, lng: 139.7},
+    });
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map);
+    const image = {
+        url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+        
+        size: new google.maps.Size(20, 32),
+        
+        origin: new google.maps.Point(0, 0),
+        
+        anchor: new google.maps.Point(0, 32),
+      };
+      
+      
+      
+    const shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: "poly",
+    };
+    map.addListener("click", (e) => {
+        clicks++;
+        if (clicks === 1) {
+            start = e.latLng;
+            // start地点にマーカーを表示
+            new google.maps.Marker({
+                position: start,
+                map: map,
+            });
+        } else if (clicks === 2) {
+            end = e.latLng;
+            clicks = 0;
+            // end地点にendマーカーを表示
+            new google.maps.Marker({
+                position: end,
+                map: map,
+                icon: image,
+                shape: shape,
+            });
+            calculateAndDisplayRoute(directionsService, directionsRenderer, start, end);
+
+        }
+    }
+    );
+    // 追加
+    
+}
+function calculateAndDisplayRoute(directionsService, directionsRenderer, start, end) {
+    const num = 10;
+    directionsService.route(
+        {
+            origin: start,
+            destination: end,
+            travelMode: google.maps.TravelMode.DRIVING,
+        },
+        (response, status) => {
+            if (status === 'OK'){
+                directionsRenderer.setDirections(response);
+                // startからendまでの10地点を取得しリストに格納
+                const route = response.routes[0].overview_path;
+                
+                const step = Math.floor(route.length / num);
+                const markers = [];
+                for (let i = 0; i < route.length; i += step) {
+                    markers.push({
+                        location: route[i],
+                    });
+                    
+                }
+                // num地点のstreetviewを地図の下に順に表示
+                const sv = new google.maps.StreetViewService();
+                markers.forEach((marker, index) => {
+                    setTimeout(() => {
+                        sv.getPanorama({location: marker.location, radius: 50}, (data, status) => {
+                            if (status === "OK") {
+                                const panorama = new google.maps.StreetViewPanorama(
+                                    document.getElementById("pano")
+                                );
+                                panorama.setPano(data.location.pano);
+                                panorama.setPov({
+                                    heading: 270,
+                                    pitch: 0,
+                                });
+                                panorama.setVisible(true);
+                            }
+                        });
+                    }, index * 3000);
+                });
+               
+                
+            }
+        }
+    );
+}
